730d3a286a55808ff9854b24fdef5964fb946b60
Mahmud Bakale <app.mahmud.bakale@gmail.com>
2025-10-06 14:20:39 +0100

    Wip

--- app/Imports/ProductsImport.php
+++ app/Imports/ProductsImport.php
@@ -13,8 +13,13 @@ class ProductsImport implements ToCollection,  WithHeadingRow
     public function collection(Collection $rows)
     {
         foreach ($rows as $row) {
-            $sellingPrice = isset($row['selling_price']) && !empty($row['selling_price']) 
-                ? $row['selling_price'] 
+            // Skip rows with empty essential fields
+            if (empty($row['product']) || empty($row['cost']) || empty($row['quantity'])) {
+                continue;
+            }
+
+            $sellingPrice = isset($row['selling_price']) && !empty($row['selling_price'])
+                ? $row['selling_price']
                 : $row['cost'] * app(StoreSettings::class)->sell_margin;
 
             Product::updateOrCreate(
